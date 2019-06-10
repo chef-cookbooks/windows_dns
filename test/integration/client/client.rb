6841aed943fa5db02aabefb032d9a1c4a1478c0b
Corey Hemminger <hemminger@hotmail.com>
2019-06-10 16:46:19 -0500

    fix tests
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/integration/client/client.rb
+++ test/integration/client/client.rb
@@ -1,3 +1,7 @@
 describe powershell('if ((Get-DnsClientGlobalSetting | select -ExpandProperty suffixsearchlist) -eq "chef.local"){$true}else{$false}') do
   its('strip') { should eq 'True' }
 end
+
+describe powershell('if ((Get-DnsClientGlobalSetting | select -ExpandProperty serversearchorder) -eq "8.8.8.8"){$true}else{$false}') do
+  its('strip') { should eq 'True' }
+end

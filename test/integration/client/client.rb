058dc96626e0792384343e7df01ebd2c2511e45c
Corey Hemminger <hemminger@hotmail.com>
2019-06-10 19:32:11 -0500

    fix integration tests
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/integration/client/client.rb
+++ test/integration/client/client.rb
@@ -1,7 +1,15 @@
-describe powershell('if ((Get-DnsClientGlobalSetting | select -ExpandProperty suffixsearchlist) -eq "chef.local"){$true}else{$false}') do
-  its('strip') { should eq 'True' }
+def wmi_call(nic_alias, method)
+  <<~EOS
+      $wmi = Get-WmiObject win32_NetworkAdapter -filter 'NetConnectionID = "#{nic_alias}"'
+      $wmi = Get-WmiObject win32_NetworkAdapterConfiguration -filter "Index=$($wmi.index)"
+      $wmi.#{method}
+  EOS
 end
 
-describe powershell('if ((Get-DnsClientGlobalSetting | select -ExpandProperty serversearchorder) -eq "8.8.8.8"){$true}else{$false}') do
-  its('strip') { should eq 'True' }
+describe powershell(wmi_call('Ethernet', 'DNSDomainSuffixSearchOrder')) do
+  its('strip') { should eq 'chef.local' }
+end
+
+describe powershell(wmi_call('Ethernet', 'DNSServerSearchOrder'))  do
+  its('strip') { should eq '8.8.8.8' }
 end

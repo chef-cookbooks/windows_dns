a4b9e4a2f365810f0ece6c99fd4f048b06e744d1
Corey Hemminger <hemminger@hotmail.com>
2019-06-10 21:10:15 -0500

    test fix attempt 2
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/integration/client/client.rb
+++ test/integration/client/client.rb
@@ -1,15 +1,16 @@
 def wmi_call(nic_alias, method)
   <<~EOS
-      $wmi = Get-WmiObject win32_NetworkAdapter -filter 'NetConnectionID = "#{nic_alias}"'
-      $wmi = Get-WmiObject win32_NetworkAdapterConfiguration -filter "Index=$($wmi.index)"
-      $wmi.#{method}
+    $wmi = Get-WmiObject win32_NetworkAdapter -filter 'NetConnectionID = "#{nic_alias}"'
+    $wmi = Get-WmiObject win32_NetworkAdapterConfiguration -filter "Index=$($wmi.index)"
+    $wmi.#{method}
   EOS
-end
-
-describe powershell(wmi_call('Ethernet', 'DNSDomainSuffixSearchOrder')) do
-  its('strip') { should eq 'chef.local' }
 end
 
 describe powershell(wmi_call('Ethernet', 'DNSServerSearchOrder')) do
   its('strip') { should eq '8.8.8.8' }
+end
+
+sleep(60)
+describe powershell(wmi_call('Ethernet', 'DNSDomainSuffixSearchOrder')) do
+  its('strip') { should eq 'chef.local' }
 end

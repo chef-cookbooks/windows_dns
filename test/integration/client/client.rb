6812e4c2a41a8ea89e80238724886adf435fd3d8
Corey Hemminger <hemminger@hotmail.com>
2019-06-10 19:35:03 -0500

    linting fix
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/integration/client/client.rb
+++ test/integration/client/client.rb
@@ -10,6 +10,6 @@ describe powershell(wmi_call('Ethernet', 'DNSDomainSuffixSearchOrder')) do
   its('strip') { should eq 'chef.local' }
 end
 
-describe powershell(wmi_call('Ethernet', 'DNSServerSearchOrder'))  do
+describe powershell(wmi_call('Ethernet', 'DNSServerSearchOrder')) do
   its('strip') { should eq '8.8.8.8' }
 end
